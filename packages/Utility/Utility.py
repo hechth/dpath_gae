d33f58b3c8ee1373fda2a1e88946c2c8de0cf336
hechth <helge.hecht@gmx.de>
2019-10-24 14:51:47 +0200

    Added utility functionality to make directory and make it public.

--- packages/Utility/Utility.py
+++ packages/Utility/Utility.py
@@ -1,4 +1,5 @@
 import glob, os
+import stat
 from sklearn.model_selection import train_test_split
 
 def collect_files(path, pattern)->list:
@@ -11,7 +12,7 @@ def collect_files(path, pattern)->list:
     """
     return glob.glob(os.path.join(path, pattern))
 
-def split_shuffle_list(data, first_list_part, random_state=42)->tuple(list, list):
+def split_shuffle_list(data, first_list_part, random_state=42)->tuple:
     """
     Function that splits a list into two lists with proportions first_list_part, 1 - first_list_part and random elements from the original list.
 
@@ -21,3 +22,19 @@ def split_shuffle_list(data, first_list_part, random_state=42)->tuple(list, list
     """
     first, second = train_test_split(data, test_size=1-first_list_part,random_state=random_state)
     return (first,second)
+
+def make_directory(path, make_public=True):
+    """
+    Function to create directory at path if it doesn't exist.
+    If make_public is True, gives rwx to all users.
+    """
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+    if make_public == True:
+        publish(path)
+
+def publish(path):
+    """
+    Function which gives every user rwx access to target at path.
+    """
+    os.chmod(path, stat.S_IROTH |stat.S_IWOTH | stat.S_IXOTH)
---
+++ packages/Utility/__init__.py
@@ -0,0 +1,5 @@
+from .Utility import collect_files
+from .Utility import split_shuffle_list
+
+from .Utility import make_directory
+from .Utility import publish
