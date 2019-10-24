f7934e7081812c470381d9fb23ae8a7e8c6f3940
hechth <helge.hecht@gmx.de>
2019-10-24 11:08:21 +0200

    added test case to test extract_image_patches

---
+++ tests/python/extract_patches.py
@@ -0,0 +1,27 @@
+import sys, os
+import git
+git_root = git.Repo('.', search_parent_directories=True).working_tree_dir
+sys.path.append(git_root)
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+import packages.Tensorflow.Image as ctfi
+
+import tensorflow as tf
+tf.enable_eager_execution()
+
+def main(argv):
+    filename = os.path.join(git_root,'tests','data','images','tile_8_14.jpeg')
+    image = ctfi.load(filename, channels=3)
+
+    patches = ctfi.extract_patches(image, 64)
+    image_patch = patches[0,:,:,:]
+    
+    fig, ax = plt.subplots()
+    plt.imshow(image_patch.numpy())
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
