5e4857869a54eaf1eece6cfd1d7545cff68030e9
hechth <helge.hecht@gmx.de>
2019-10-24 16:54:38 +0200

    Implemented functionality to extract parameters from model identifier.

---
+++ packages/Tensorflow/Model/SavedModel.py
@@ -0,0 +1,33 @@
+import sys
+import git
+git_root = git.Repo('.', search_parent_directories=True).working_tree_dir
+sys.path.append(git_root)
+
+from packages.Utility import match_regex
+
+def determine_patch_size(model_dir)->int:
+    """
+    Function which extracts information about the patch size from the model directory string.
+    Returns
+    -------
+    patch_size: int
+    """
+    return int(match_regex('(p{1}[0-9]+)(?=-)',model_dir)[1:])
+
+def determine_batch_size(model_dir)->int:
+    """
+    Function which extracts information about the batch size from the model directory string.
+    Returns
+    -------
+    batch_size: int
+    """
+    return int(match_regex('(b{1}[0-9]+)',model_dir)[1:])
+
+def determine_latent_space_size(model_dir)->int:
+    """
+    Function which extracts information about the latent_space_size from the model directory string.
+    Returns
+    -------
+    latent_space_size: int
+    """
+    return int(match_regex('(l{1}[0-9]+)(?=-)',model_dir)[1:])
---
+++ packages/Tensorflow/Model/__init__.py
@@ -0,0 +1,3 @@
+from .SavedModel import determine_batch_size
+from .SavedModel import determine_patch_size
+from .SavedModel import determine_latent_space_size
