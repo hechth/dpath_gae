3b0574d8fbf5b40e16a5f79685531a91b6808546
hechth <helge.hecht@gmx.de>
2019-10-24 11:07:46 +0200

    Added functionality to extract patches from images.

--- packages/Tensorflow/Image/Image.py
+++ packages/Tensorflow/Image/Image.py
@@ -29,3 +29,64 @@ def rescale(image, new_min, new_max):
     image:  Image with intensity values between new_min and new_max and same shape.
     """
     return np.interp(image, (image.min(), image.max()), (new_min, new_max))
+
+def extract_patches(
+    image,
+    patch_size,
+    padding="VALID",
+    rates=[1,1,1,1],
+    strides=None,
+    ksizes=None):
+
+    """
+    Function which splits image of shape [width, height, 3] into patches with more convenient ordering than the built in tensorflow function
+    tf.image.extract_image_patches (https://www.tensorflow.org/versions/r1.12/api_docs/python/tf/image/extract_image_patches)
+
+    Returns
+    -------
+    patches: image patches of shape [num_patches, patch_size, patch_size, 3]
+    """
+
+    # Set strides and ksizes if not passed as arguments
+    if strides is None:
+        strides=[1, patch_size, patch_size, 1]
+
+    if ksizes is None:
+        ksizes=[1, patch_size, patch_size, 1]
+    
+    # Split image into rgb channels
+    r,g,b = tf.split(image, 3, 2)       
+
+    # Extract patches from the image using tensorflow function
+    patches = tf.image.extract_image_patches(
+      [r,g,b],
+      ksizes,
+      strides,
+      rates,
+      padding
+    )
+
+    patches = tf.expand_dims(patches, 0)
+    num_patches = [int(image.shape.as_list()[0] / patch_size), int(image.shape.as_list()[1] / patch_size)]
+
+    # Change grid layout of [1, channels, x, y, pixels] to [patches, channels, 1, 1, pixels]
+    patches = tf.space_to_batch_nd(
+      patches,
+      [1, num_patches[0], num_patches[1]],
+      [[0, 0], [0, 0], [0, 0]]
+    )
+
+    # Squeeze [patches, channels, 1, 1, pixels] to [patches, channels, pixels]
+    patches = tf.squeeze(patches)
+
+    # Change shape to channels last: [patches, channels, pixels] to [patches, pixels, channels]
+    patches = tf.transpose(patches, perm=[0,2,1])
+
+    # Reshape from [patches, pixels, channels] to [patches, rows, cols, channels]
+    patches = tf.reshape(
+      patches,
+      [patches.shape[0], patch_size, patch_size, patches.shape[2]]
+    )
+
+    return patches
+    
--- packages/Tensorflow/Image/__init__.py
+++ packages/Tensorflow/Image/__init__.py
@@ -1,2 +1,3 @@
 from .Image import load
 from .Image import rescale
+from .Image import extract_patches
