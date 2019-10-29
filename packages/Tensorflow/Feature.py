88bc6f9f0550e4f430866014122d127a36188e93
hechth <helge.hecht@gmx.de>
2019-10-29 15:19:51 +0100

    Added float feature function.

--- packages/Tensorflow/Feature.py
+++ packages/Tensorflow/Feature.py
@@ -6,3 +6,9 @@ def string_feature(string):
     Function which returns tf.train.Feature for given string.
     """
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[string.encode()]))
+
+def float_feature(value):
+    """
+    Function which returns tf.train.Feature for given float.
+    """
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
--- packages/Tensorflow/__init__.py
+++ packages/Tensorflow/__init__.py
@@ -1,4 +1,5 @@
 from .Feature import string_feature
+from .Feature import float_feature
 
 from .Maps import tf_datatypes
 from .Maps import tf_operations
