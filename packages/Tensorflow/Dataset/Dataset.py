72fa8d416b7dd6d75dbdfe1dc7f725ca6cc4dbec
hechth <helge.hecht@gmx.de>
2019-10-29 15:20:22 +0100

    Added functionality to construct zip and decode ops for dataset

--- packages/Tensorflow/Dataset/Dataset.py
+++ packages/Tensorflow/Dataset/Dataset.py
@@ -46,3 +46,78 @@ def write(data, func_encode, filename):
     # Flush and close the writer.
     writer.flush()
     writer.close()
+
+def construct_decode_op(cfg_inputs):
+    """
+    Function that based on the feature descriptions in a config file constructs the decode operation
+    which deserializes the samples into a dict mapping the keys to the features
+
+    Parameters
+    ----------
+    cfg_inputs: dict describing input configuration with 'features' and 'labels'
+    """
+
+    cfg_features = cfg_inputs['features']
+    cfg_labels = cfg_inputs['labels']
+
+    def decode_example(example_proto):
+        """
+        Function to decode an example by parsing the config to create the feature description.
+
+        Parameters
+        ----------
+        example_proto: a tf.train.Example
+
+        Returns
+        -------
+        dict mapping the decoded features
+        """   
+        desc = {}
+        for entry in cfg_features:
+            desc[entry['key']] = tf.FixedLenFeature(entry['shape'], entry['dtype'])
+        
+        desc[cfg_labels['key']] = tf.FixedLenFeature(cfg_labels['shape'], cfg_labels['dtype'])
+
+        return tf.parse_single_example(example_proto, desc)
+    return decode_example
+
+def construct_unzip_op(cfg_inputs):
+    """
+    Function that based on the feature descriptions in a config file constructs the final mapping operation
+    which transforms the data in the right format.
+
+    Parameters
+    ----------
+    cfg_inputs: dict describing input configuration with 'features' and 'labels'
+
+    Returns
+    -------
+    unzip_example: callable which returns a pair (features, label).
+    """
+
+    cfg_features = cfg_inputs['features']
+    cfg_labels = cfg_inputs['labels']
+
+    def unzip_example(example_proto):
+        """
+        Function that transforms a sample of the from
+        { 'f0': val0, 'f1': val1, ..., 'label': val_label }
+        to
+        ({ 'f0': val0, 'f1': val1, ..., 'fx': valx }, val_label)
+
+        Parameters
+        ----------
+        example_proto: dict of form { 'f0': val0, 'f1': val1, ..., 'label': val_label }
+
+        Returns
+        -------
+        Pair of form ({ 'f0': val0, 'f1': val1, ..., 'fx': valx }, val_label)
+        """   
+        features = {}
+        for entry in cfg_features:
+            features[entry['key']] = example_proto[entry['key']]
+        label = example_proto[cfg_labels['key']]
+
+        return (features, label)
+
+    return unzip_example
--- packages/Tensorflow/Dataset/__init__.py
+++ packages/Tensorflow/Dataset/__init__.py
@@ -2,3 +2,6 @@ from .Dataset import write
 
 from .Dataset import encode
 from .Dataset import decode
+
+from .Dataset import construct_decode_op
+from .Dataset import construct_unzip_op
