4fc75025dd0f5914597216c6346200a0cf22403b
hechth <helge.hecht@gmx.de>
2019-10-31 18:24:01 +0100

    Continued on gae model and tested dataset pipeline for this case.

--- examples/models/gae/gae_model.py
+++ examples/models/gae/gae_model.py
@@ -21,7 +21,7 @@ def my_model(features, labels, mode, params, config):
         components[comp['name']] = ctfm.parse_component(tensors, comp, tensors)
 
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.01)
-    loss = tf.losses.absolute_difference(tensors['val'], tensors['logits'])
+    loss = tf.losses.absolute_difference(tensors['patch'], tensors['logits'])
 
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
@@ -32,7 +32,28 @@ def my_model(features, labels, mode, params, config):
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 def main(argv):
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('model_dir',type=str,help='Path to saved model to use for inference.')
+    args = parser.parse_args()
 
+    config = ctfm.parse_json(os.path.join(git_root,'examples','models','gae','configuration.json'))
+
+    train_fn = ctfd.construct_train_fn(config['datasets'])
+    steps = int(config['datasets']['training']['size'] / config['datasets']['batch'])
+
+    params_dict = {
+        'config': config['model'],
+        'model_dir': args.model_dir
+    }
+
+    classifier = tf.estimator.Estimator(
+      model_fn=my_model,
+      model_dir=args.model_dir,
+      params=params_dict,
+      config=tf.estimator.RunConfig(model_dir=args.model_dir, save_summary_steps=100, log_step_count_steps=100)
+    )
+
+    classifier = classifier.train(input_fn=train_fn, steps=steps)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
