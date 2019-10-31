36ae821d1f254df095d75e936e278983a1a448eb
hechth <helge.hecht@gmx.de>
2019-10-31 12:00:01 +0100

    Fixed bug in autoencoder example.

--- examples/models/autoencoder/autoencoder.py
+++ examples/models/autoencoder/autoencoder.py
@@ -14,14 +14,14 @@ import packages.Tensorflow.Dataset as ctfd
 def my_model(features, labels, mode, params, config):
     cfg = params['config']
 
-    inputs, labels = ctfm.parse_inputs(features, labels, cfg['inputs'])
+    tensors, labels = ctfm.parse_inputs(features, labels, cfg['inputs'])
     components = {}
    
     for comp in cfg['components']:
-        components[comp['name']] = ctfm.parse_component(inputs, comp, inputs)
+        components[comp['name']] = ctfm.parse_component(tensors, comp, tensors)
 
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.01)
-    loss = tf.losses.absolute_difference(inputs['patch'], inputs['logits'])
+    loss = tf.losses.absolute_difference(tensors['val'], tensors['logits'])
 
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
@@ -31,8 +31,23 @@ def my_model(features, labels, mode, params, config):
     assert mode == tf.estimator.ModeKeys.TRAIN   
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
+def train_func():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+    x_train = x_train.astype('float32') / 255
 
+    # Reshape input data from (28, 28) to (28, 28, 1)
+    w, h = 28, 28
+    x_train = x_train.reshape(x_train.shape[0], w, h, 1)
+
+    # One-hot encode the labels
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    
+    train_ds = tf.data.Dataset.zip((tf.data.Dataset.from_tensor_slices(x_train),tf.data.Dataset.from_tensor_slices(y_train)))
+    train_ds = train_ds.map(lambda image, label: ({"val": image }, label))
+
+    train_ds = train_ds.repeat().shuffle(60000).batch(10)
+    return train_ds
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Compute latent code for image patch by model inference.')
@@ -42,7 +57,7 @@ def main(argv):
     model_dir = args.export_dir
 
     config_filename = os.path.join(git_root, 'examples','models','autoencoder','autoencoder.json')    
-    cfg = ctfm.parse_json(config_filename)
+    cfg = ctfm.parse_json(config_filename)['model']
 
 
     params_dict = {
@@ -57,7 +72,7 @@ def main(argv):
       config=tf.estimator.RunConfig(model_dir=model_dir, save_summary_steps=100, log_step_count_steps=100)
     )
 
-    classifier = classifier.train(input_fn=ctfd.construct_train_fn(cfg['inputs']), steps=cfg['inputs']['datasets']['training']['steps'])
+    classifier = classifier.train(input_fn=train_func, steps=10000)
 
 
 
