5be384e5d84b07b2f1f47c5638406739a803f6da
hechth <helge.hecht@gmx.de>
2019-11-04 16:29:27 +0100

    Made parse_layer function public

--- packages/Tensorflow/Model/__init__.py
+++ packages/Tensorflow/Model/__init__.py
@@ -3,6 +3,7 @@ from .SavedModel import determine_patch_size
 from .SavedModel import determine_latent_space_size
 
 from .Layer import avg_unpool2d
+from .Layer import parse_layer
 
 from .Configuration import parse_component
 from .Configuration import parse_json
