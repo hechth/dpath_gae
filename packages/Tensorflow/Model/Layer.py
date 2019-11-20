Fixed bug related to diagonals of covariance matrix.

packages/Tensorflow/Model/Layer.py
@@ -312,9 +312,9 @@ def _parse_sampler_v2(input_shape, config):
 
     
     cov = tf.layers.Dense(
-        (dims * (dims+1))/2,
-        name=name + '_covariance_root',
-        activation=tf.math.sigmoid,
+        (dims * (dims-1))/2,
+        name=name + '_covariance_lower_tri',
+        activation=tf.nn.relu,
         kernel_initializer=tf.initializers.lecun_uniform(),
         bias_initializer=tf.ones_initializer())
 
@@ -325,7 +325,11 @@ def _parse_sampler_v2(input_shape, config):
     
     def _sample(x):
         mean_activation = mean(x)        
-        x_cov_tril = tf.contrib.distributions.fill_triangular(cov(x), name=name + '_covariance')        
+        x_cov_tril = tf.contrib.distributions.fill_triangular(cov(x))
+        x_cov_tril = tf.concat([x_cov_tril, tf.zeros([tf.shape(x)[0], dims - 1, 1], dtype=tf.float32)], 2)
+        x_cov_tril = tf.concat([tf.zeros([tf.shape(x)[0], 1, dims], dtype=tf.float32), x_cov_tril], 1)
+
+        x_cov_tril = tf.linalg.set_diag(x_cov_tril, tf.ones([tf.shape(x)[0], dims], dtype=tf.float32), name=name + '_covariance')     
 
         dist = tf.contrib.distributions.MultivariateNormalTriL(loc=mean_activation, scale_tril=x_cov_tril,name='x_dist')
         sample = dist.sample()