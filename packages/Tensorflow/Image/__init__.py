4b57c2b980e11cfa02028f90106ea40c612552fd
hechth <helge.hecht@gmx.de>
2019-12-11 11:31:25 +0100

    Reworked reconstruction visualization scripts.

--- packages/Tensorflow/Image/__init__.py
+++ packages/Tensorflow/Image/__init__.py
@@ -1,5 +1,6 @@
 from .Image import load
 from .Image import rescale
 from .Image import extract_patches
+from .Image import stitch_patches
 from .Image import subsample
 from .Image import is_image
--- tools/model/visualization/plot_reconstruction.py
+++ tools/model/visualization/plot_reconstruction.py
@@ -7,8 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from tensorflow.contrib import predictor
-
 import packages.Tensorflow as ctf
 import packages.Tensorflow.Model as ctfm
 import packages.Tensorflow.Image as ctfi
@@ -16,16 +14,13 @@ import packages.Utility as cutil
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description='Compute codes and reconstructions for image.')
+    parser = argparse.ArgumentParser(description='Plot image and its reconstruction.')
     parser.add_argument('export_dir',type=str,help='Path to saved model.')
     parser.add_argument('mean', type=str, help='Path to npy file holding mean for normalization.')
     parser.add_argument('variance', type=str, help='Path to npy file holding variance for normalization.')
     parser.add_argument('filename', type=str,help='Image file or numpy array to run inference on.')
     parser.add_argument('image_size', type=int, nargs=2,help='Size of the image, HW.')
     parser.add_argument('patch_size', type=int, help='Size of image patches.')
-    parser.add_argument('stride', type=int, help='Size of stride.')
-    parser.add_argument('codes_out', type=str,help='Where to store the numpy array of codes.')
-    parser.add_argument('reconstructions_out', type=str,help='Where to store the numpy array of reconstructions.')
     args = parser.parse_args()
 
     mean = np.load(args.mean)
@@ -34,8 +29,8 @@ def main(argv):
 
     def denormalize(image):
         channels = [np.expand_dims(image[:,:,channel] * stddev[channel] + mean[channel],-1) for channel in range(3)]
-        denormalized_image = ctfi.rescale(np.concatenate(channels, 2), 0.0, 1.0)
-        return denormalized_image
+        denormalized_image = np.concatenate(channels, 2)
+        return ctfi.rescale(denormalized_image,0.0, 1.0)
 
     def normalize(image, name=None):
         channels = [tf.expand_dims((image[:,:,:,channel] - mean[channel]) / stddev[channel],-1) for channel in range(3)]
@@ -46,24 +41,24 @@ def main(argv):
 
     with tf.Session(graph=tf.get_default_graph()).as_default() as sess:
         image = ctfi.load(args.filename,height=args.image_size[0], width=args.image_size[1])
-        patches = normalize(ctfi.extract_patches(image, args.patch_size, strides=[1,args.stride,args.stride,1]))
+        strides = [1, args.patch_size, args.patch_size, 1]
+        patches = normalize(ctfi.extract_patches(image, args.patch_size, strides=strides))
+        reconstructions = tf.contrib.graph_editor.graph_replace(sess.graph.get_tensor_by_name('imported/logits:0') ,{ sess.graph.get_tensor_by_name('imported/patch:0'): patches })
+        reconstructed_image = tf.squeeze(ctfi.stitch_patches(reconstructions, strides, args.image_size))
 
-        codes = tf.contrib.graph_editor.graph_replace(sess.graph.get_tensor_by_name('imported/code:0') ,{ sess.graph.get_tensor_by_name('imported/patch:0'): patches })
-        reconstructions = tf.contrib.graph_editor.graph_replace(sess.graph.get_tensor_by_name('imported/logits:0') ,{ sess.graph.get_tensor_by_name('imported/code:0'): codes })
-        
+        sess.run(tf.global_variables_initializer())
         saver.restore(sess, latest_checkpoint)
 
-        codes_npy = sess.run(codes)
-        reconstructions_npy = np.array(list(map(denormalize,sess.run(reconstructions))))
+        image_eval = sess.run(image)
+        reconstructed_image_eval = sess.run(reconstructed_image)
 
-        plt.imshow(denormalize(sess.run(ctfi.stitch_patches(reconstructions,[1,args.stride,args.stride,1], args.image_size))))
+        fig, ax = plt.subplots(1,2)
+        ax[0].imshow(image_eval)
+        ax[1].imshow(denormalize(reconstructed_image_eval))
+
         plt.show()
-        
-        np.save(args.codes_out,codes_npy)
-        np.save(args.reconstructions_out, reconstructions_npy)
-        print("Done!")
-                
-
+        sess.close()
+    print("Done!")
 
 if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.INFO)
